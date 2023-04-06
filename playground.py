b1dddb9793f8f4520ed2fc7ada17535a70d441b6
Ayoodumak <chemathphy@gmail.com>
2023-04-06 19:47:03 +0100

    added gradient accumulation

--- playground.py
+++ playground.py
@@ -2,6 +2,12 @@
 # 2. Play around with the concept of dataloader and batch tokenization
 # 3. Run a sample train loop while configuring every part of the GPT experiment
 
+# TODO
+# 1. Create a learning rate scheduler
+# 2. Write evaluation and generation function to see output of model
+# 3. Optimize the DataLoader by using pytorch dataloader or add multiprocessing to the current one
+# 4. Make the hyperparameters into a dataclass so as to make them flexible
+
 import wandb
 import torch 
 import torch.nn as nn
@@ -18,16 +24,17 @@ wandb.login()
 wandb.init(project="GPT1-exp")
 
 # Hyperparameters
-VOCAB_SIZE = 10000
+VOCAB_SIZE = 10000 
 EMBEDDING_DIM = 768
-NUM_OF_LAYERS = 12
+NUM_OF_LAYERS = 12 # change to 6 if the GPU runs out of memory
 SEQ_LENGTH = 512
 NUM_OF_HEADS = 12
 DROP_PROBABILITY = 0.1
 EPOCHS = 10
 LEARNING_RATE = 1e-4
-BATCH_SIZE = 32
+BATCH_SIZE = 64
 CONTEXT_SIZE = 512
+NUM_ACCUMULATION_STEPS = 4 
 
 gpt1 = GPT_1(vocab_size=VOCAB_SIZE, embedding_dim=EMBEDDING_DIM, num_of_layers=NUM_OF_LAYERS, 
             seq_length=SEQ_LENGTH, num_of_heads=NUM_OF_HEADS, dropout_probability=DROP_PROBABILITY).to(device)
@@ -77,23 +84,32 @@ train_iter = iter(train_dataloader.get_batch())
 criterion = nn.NLLLoss()
 optimizer = torch.optim.Adam(gpt1.parameters(), lr=LEARNING_RATE)
 
-for x, y in train_iter:
-    x = x.to(device)
-    y = y.to(device)
+# Refactor this
+def train():
     
-    gpt1.zero_grad()
-    
-    y_pred = gpt1(x)
-    
-    loss = criterion(y_pred.view(-1, y_pred.size(-1)), y.view(-1))
-    
-    loss.backward()
-    
-    optimizer.step()
-    
-    print(f"Train loss: {loss.item()}")
-    
-    wandb.log({"loss": loss.item()})
+    for i, (x, y) in enumerate(train_iter):
+        x = x.to(device)
+        y = y.to(device)
+        
+        optimizer.zero_grad()
+        
+        y_pred = gpt1(x)
+        
+        loss = criterion(y_pred.view(-1, y_pred.size(-1)), y.view(-1))
+        
+        loss = loss / NUM_ACCUMULATION_STEPS
+        loss.backward()
+        
+        if ((i + 1) % NUM_ACCUMULATION_STEPS == 0) or (i + 1 == len(train_iter)):
+            # Update weights
+            optimizer.step()
+        
+        print(f"Train loss: {loss.item()}")
+        
+        wandb.log({"loss": loss.item()})
+        
+
+if __name__ == "__main__":
+    train()
     
 
-    
