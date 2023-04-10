fbad024cd6bc27a93471345626d8f3bba5f51a94
Ayoodumak <chemathphy@gmail.com>
2023-04-10 14:10:02 +0100

    added training interface for tokenizer

---
+++ preprocessing/train_tokenizer.py
@@ -0,0 +1,23 @@
+from tokenizer import BytePairTokenizer
+from preprocessing import read_file
+import argparse
+
+
+# CLI parser
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+parser.add_argument("corpus_path", type=str, help="Path to corpus you want to train tokenizer on")
+parser.add_argument("vocab_size", type=int, help="Vocabulary size.")
+parser.add_argument("--out", type=str, default="tokenizer.json", help="output path to save the tokenizer after training")
+
+opt = parser.parse_args()
+
+# Read the file
+corpus = read_file(opt.corpus_path)
+
+# Create tokenizer
+tokenizer = BytePairTokenizer(opt.vocab_size)
+
+# Train tokenizer and save as tokenizer.json
+tokenizer.train(corpus, opt.out)
+
