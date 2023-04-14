c6612c3cfdcc3833ffe43d816f445c6e3e3078e1
Ayoodumak <chemathphy@gmail.com>
2023-04-14 09:22:54 +0100

    added top-k sampling for generation

--- gpt.py
+++ gpt.py
@@ -20,3 +20,31 @@ class GPT_1(nn.Module):
         
         return nn.functional.log_softmax(self.linear(output), dim=-1)
     
+    @torch.no_grad()
+    def generate(self, input_context, max_token, topk):
+        # This implements the top-k sampling scheme
+        predicted_tokens = []
+        for _ in range(max_token):
+            input_tensor = torch.LongTensor([input_context])
+            # print(input_tensor.shape)
+
+            # Get the output 
+            y_pred = self.forward(input_tensor)
+
+            # Crop the last prediction
+            y_pred = y_pred[:, -1, :]
+    
+            v, _ = torch.topk(y_pred, k=topk)
+            y_pred[y_pred < v[:, [-1]]] = -float("Inf")
+    
+            probs = nn.functional.softmax(y_pred)
+            idx = torch.multinomial(probs, num_samples=1)
+            
+            predicted_tokens.append(idx.item())
+            
+            input_context.append(idx.item())
+            input_context = input_context[1:]
+            
+        return predicted_tokens
+    
+    
