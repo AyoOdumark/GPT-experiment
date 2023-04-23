a675220b1e041fd773e8bc3ca0962eda666bc928
Ayoodumak <chemathphy@gmail.com>
2023-04-23 17:59:14 +0100

    add option for byte-level BPE

--- preprocessing/train_tokenizer.py
+++ preprocessing/train_tokenizer.py
@@ -1,4 +1,4 @@
-from tokenizer import BytePairTokenizer
+from tokenizer import BytePairTokenizer, BBPETokenizer
 from preprocessing import read_file
 import argparse
 
@@ -8,12 +8,18 @@ parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFo
 
 parser.add_argument("corpus_path", type=str, help="Path to corpus you want to train tokenizer on")
 parser.add_argument("vocab_size", type=int, help="Vocabulary size.")
+parser.add_argument("--tokenizer", type=str, default="bpe", help="Choose tokenization scheme. Write bbpe for byte-level BPE or bpe for vanilla BPE")
 parser.add_argument("--out", type=str, default="tokenizer.json", help="output path to save the tokenizer after training")
 
 opt = parser.parse_args()
 
 # Create tokenizer
-tokenizer = BytePairTokenizer(opt.vocab_size)
+if opt.tokenizer == "bpe":
+    tokenizer = BytePairTokenizer(opt.vocab_size)
+elif opt.tokenizer == "bbpe":
+    tokenizer = BBPETokenizer(opt.vocab_size)
+else:
+    print("Enter a valid tokenizer.")
 
 # Train tokenizer and save as tokenizer.json
 tokenizer.train(opt.corpus_path, opt.out)
